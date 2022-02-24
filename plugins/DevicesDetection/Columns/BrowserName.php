2cd0672da52ce18ac34e506bf961d880b272392d
sgiehl <stefan@matomo.org>
2022-02-24 17:30:59 +0100

    use client hints for detection

--- plugins/DevicesDetection/Columns/BrowserName.php
+++ plugins/DevicesDetection/Columns/BrowserName.php
@@ -77,8 +77,7 @@ class BrowserName extends Base
      */
     public function onNewVisit(Request $request, Visitor $visitor, $action)
     {
-        $userAgent = $request->getUserAgent();
-        $parser    = $this->getUAParser($userAgent);
+        $parser    = $this->getUAParser($request->getUserAgent(), $request->getClientHints());
 
         $aBrowserInfo = $parser->getClient();
 
--- plugins/DevicesDetection/Columns/DeviceType.php
+++ plugins/DevicesDetection/Columns/DeviceType.php
@@ -51,8 +51,7 @@ class DeviceType extends Base
      */
     public function onNewVisit(Request $request, Visitor $visitor, $action)
     {
-        $userAgent = $request->getUserAgent();
-        $parser    = $this->getUAParser($userAgent);
+        $parser    = $this->getUAParser($request->getUserAgent(), $request->getClientHints());
 
         return $parser->getDevice();
     }
