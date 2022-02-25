765257823034c5b834dad733148efa1d4a8b0d13
sgiehl <stefan@matomo.org>
2022-02-25 16:10:15 +0100

    don't use catch, as yui compressor can't parse it

--- js/piwik.js
+++ js/piwik.js
@@ -3005,7 +3005,7 @@ if (typeof window.Matomo !== 'object') {
                     appendix += '&uadata=' + JSON.stringify(ua);
 
                     if (request instanceof Array) {
-                        for (i = 0; i < request.length; i++) {
+                        for (var i = 0; i < request.length; i++) {
                             request[i] += appendix;
                         }
                     } else {
@@ -3013,7 +3013,7 @@ if (typeof window.Matomo !== 'object') {
                     }
 
                     callback(request);
-                }).catch(function (message) {
+                }, function (message) {
                     callback(request);
                 });
             }
